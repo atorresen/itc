16281386e61006938544aab82989f9f847ad7d81
atorresen <torresen@umich.edu>
2026-01-09 17:59:33 -0500

    Add example p5js sketch hookup

---
+++ lessons/example_p5_sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    // define a variable to hold your canvas
+    var myCanvas = createCanvas(400, 400);
+
+    //place your canvas inside the parent <div> with id="mySketch"
+    myCanvas.parent("mySketch");
+}
+
+function draw() {
+    background("green");
+}
